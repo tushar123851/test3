71e1679b481f77e23500cd4341a6d4baba043e78
tushar123851 <tusharvala707@gmail.com>
2025-05-10 13:40:41 +0530

    Add files via upload

--- test3.py
+++ test3.py
@@ -1,14 +1,24 @@
+# Welcome message
 print("Welcome To Pattern Generator And Number Analyer!")
 
+# Infinite loop for menu
 while True:
+     # Display menu options
+    print("\n")
     print("Select an option:")
     print("1. Generate pattern")
     print("2. Analyze a range of numbers")
     print("3. Exit")
 
+     # Take user input for menu choice
     choice = input("enter your choice:")
 
     match choice:
+      # Option 1: Display Pattern according user choose 
+      # 1.right angled tringle 
+      # 2.pyramid 
+      # 3.Left angled tringle 
+
         case "1":
             
             
@@ -18,18 +28,41 @@ while True:
             print("3. Left angled Tringle")
 
             pattern_choice = input("enter your choice:")
-
+ 
             match pattern_choice:
 
              case "1":
-                  break
+                rows = int(input("Enter number of rows for pattern:"))
+                print("\n")
+                print("pattern:")
+               
+                for i in range(1,rows+1):
+                 print("*  " * i)
+              
              case "2":
-                    break
+                rows = int(input("Enter number of rows for pattern:"))
+                print("\n")
+                print("pattern:")
+                
+
+                for i in range(1,rows + 1):
+                  print(" " * (rows - i) + "* "*i)
+                  
+                  
              case "3":
-                    break
-             case "4":
-                   print("exit")
-             
+                rows = int(input("Enter number of rows for the pattern: "))
+                print("\nPattern:")
+
+                for i in range(1, rows + 1):
+                    print(" " * (rows - i), end="")
+                    print("*" * i)
+
+                  
+                  
+                
+      # Option 2:display even and odd number
+      #display sum
+  
         case "2":
               sum = 0
               start_range = int(input("Enter the start of range:"))
@@ -45,14 +78,19 @@ while True:
               
               print(f"Sum of all numbers from {start_range} to {end_range}:{sum}")
     
-
+     # Option 3: exit message
         case "3":
               print("Exiting Program.GoodBye!") 
               break
-
+        
+     # Invalid input
         case _:
             print("Invalid choice. Please enter a valid option.")
               
+
+
+             
+
 
 
              
